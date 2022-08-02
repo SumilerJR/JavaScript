edcee3d1a6afc27f8a763c1b436a9262bf4849b1
九日 <mail@sumiler.com>
2022-08-02 16:55:26 +0800

    测试提交

---
+++ Ajax/server.js
@@ -0,0 +1,37 @@
+// const { urlencoded } = require('express');
+const express = require('express');
+
+//创建app实例对象
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(express.static(__dirname + '/src'));
+
+app.get('/test_get', (request, response) => {
+    console.log('有人请求test_get了，携带的query参数是', request.query);
+    response.send('hello_test_get!!!');
+})
+
+app.get('/test_get2/:name/:age', (request, response) => {
+    console.log('有人请求test_get2了，携带的params参数是', request.params);
+    response.send('hello_test_get2');
+})
+
+
+app.post('/test_post', (request, response) => {
+    console.log('有人请求test_post了', request.body);
+    response.send('hello_test_post');
+})
+
+
+app.listen(8080, (err) => {
+    if (!err) {
+        console.log('测试ajax请求的服务器开启成功了！测试地址如下：');
+        console.log('http://127.0.0.1:8080/1_ajax小试牛刀.html');
+        console.log('http://127.0.0.1:8080/2_xhr的5种状态.html');
+        console.log('http://127.0.0.1:8080/3_ajax_get请求.html');
+        console.log('http://127.0.0.1:8080/4_ajax_post请求.html');
+    }
+
+})
