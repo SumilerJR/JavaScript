af143987d4e605ac9b43c257682394c371d2ad80
九日 <mail@sumiler.com>
2022-08-04 07:33:15 +0800

    main

--- ES6-11/ES6/ES6_Promise/6-Promise实践-读取多个文件.js
+++ ES6-11/ES6/ES6_Promise/6-Promise实践-读取多个文件.js
@@ -1,10 +1,10 @@
 //引入 fs 模块
 const fs = require("fs");
 
-// fs.readFile('./resources/为学.md', (err, data1)=>{
-//     fs.readFile('./resources/插秧诗.md', (err, data2)=>{
-//         fs.readFile('./resources/观书有感.md', (err, data3)=>{
-//             let result = data1 + '\r\n' +data2  +'\r\n'+ data3;
+// fs.readFile('./resources/为学.md', (err, data1) => {
+//     fs.readFile('./resources/插秧诗.md', (err, data2) => {
+//         fs.readFile('./resources/观书有感.md', (err, data3) => {
+//             let result = data1 + '\r\n' + data2 + '\r\n' + data3;
 //             console.log(result);
 //         });
 //     });
@@ -13,14 +13,14 @@ const fs = require("fs");
 //使用 promise 实现
 const p = new Promise((resolve, reject) => {
     fs.readFile("./resources/为学.md", (err, data) => {
-        resolve(data);
+        resolve([data]);
     });
-});
-
-p.then(value => {
+}).then(value => {
     return new Promise((resolve, reject) => {
         fs.readFile("./resources/插秧诗.md", (err, data) => {
-            resolve([value, data]);
+            // resolve([value, data]);
+            value.push(data);
+            resolve(value);
         });
     });
 }).then(value => {
