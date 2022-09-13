c0bb9d3f0467daaee759fba7fad38326f59bc4da
九日 <mail@sumiler.com>
2022-09-13 23:42:44 +0800

    2022年9月13日23:42:41

--- Vuejs/vue_test/src/App.vue
+++ Vuejs/vue_test/src/App.vue
@@ -1,18 +1,17 @@
 <template>
-	<div>
-		<Test/>
-		<Test2/>
-		<Test3/>
+	<div class="container">
+		<Search></Search>
+		<List></List>
 	</div>
 </template>
 
 <script>
-	import Test from './components/Test'
-	import Test2 from './components/Test2'
-	import Test3 from './components/Test3'
-
+	// import axios from "axios";
+	import Search from "./components/Search";
+	import List from "./components/List";
 	export default {
-		name:'App',
-		components:{Test,Test2,Test3},
-	}
+		name: "App",
+		components: { Search, List },
+		methods: {},
+	};
 </script>
--- Vuejs/vue_test/src/main.js
+++ Vuejs/vue_test/src/main.js
@@ -1,15 +1,15 @@
 //引入Vue
-import Vue from 'vue'
+import Vue from 'vue';
 //引入App
-import App from './App.vue'
+import App from './App.vue';
 //关闭Vue的生产提示
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 //创建vm
 new Vue({
-	el:'#app',
+	el: '#app',
 	render: h => h(App),
 	beforeCreate() {
-		Vue.prototype.$bus = this
+		Vue.prototype.$bus = this;
 	},
-})
+});
