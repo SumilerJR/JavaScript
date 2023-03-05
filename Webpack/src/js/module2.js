eb02d83dfc83f624b49a31f2e0ae7fa22222a63c
九日 <mail@sumiler.com>
2023-03-05 19:21:30 +0800

    frontend basic learning

--- Webpack/src/js/module2.js
+++
@@ -1,5 +0,0 @@
-//module2使用统一暴露
-
-const data = 'abcd';
-const msg = 'hello, 123';
-export { data, msg as message };
--- Webpack/src/js/module3.js
+++
@@ -1,6 +0,0 @@
-//module3使用默认暴露
-export default {
-    name: 'cde',
-    address: '广州',
-    subjects: ['前端', 'Java', '数据结构']
-};
